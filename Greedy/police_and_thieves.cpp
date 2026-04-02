82068d8375542aa3cfb141eda084b70b5b3e44df
Sachin Kumar <sk02265532014@gmail.com>
2026-04-02 09:48:29 +0530

    Refactor greedy two-pointer solution with improved intuition, approach explanation, and consistency

--- Greedy/police_and_thieves.cpp
+++ Greedy/police_and_thieves.cpp
@@ -24,3 +24,40 @@
 // Space Complexity: O(n)
 
 // Tags: Greedy, Two Pointers, Array
+class Solution {
+public:
+    int catchThieves(vector<char>& arr, int k) {
+        vector<int> police, thieves;
+
+        // Collect positions of policemen and thieves
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == 'P')
+                police.push_back(i);
+            else if (arr[i] == 'T')
+                thieves.push_back(i);
+        }
+
+        int i = 0, j = 0;
+        int caught = 0;
+
+        // Greedy matching using two pointers
+        while (i < police.size() && j < thieves.size()) {
+            if (abs(police[i] - thieves[j]) <= k) {
+                // Valid match
+                caught++;
+                i++;
+                j++;
+            }
+            else if (thieves[j] < police[i]) {
+                // Thief is too far left, move thief pointer
+                j++;
+            }
+            else {
+                // Policeman is too far left, move police pointer
+                i++;
+            }
+        }
+
+        return caught;
+    }
+};
